2a8a9ad33086cd38db84a21794ba11aa4c67e5a4
prasannakarki77 <prasanna.karki77@gmail.com>
2022-09-09 11:42:04 +0545

    fixed rendering issue during app start

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -7,20 +7,24 @@ const Quote = () => {
   const { getQuotesBtnHandler, randomQuote } = useContext(QuotesContext);
   const { content, author, tags } = randomQuote;
   return (
-    <div>
-      <div className="quote-details">
-        <div className="quote">
-          <p className="quote__text">{content}</p>
+    <>
+      {randomQuote.length && (
+        <div>
+          <div className="quote-details">
+            <div className="quote">
+              <p className="quote__text">{content}</p>
+            </div>
+          </div>
+          <div className="author-container" onClick={getQuotesBtnHandler}>
+            <div className="author">
+              <p className="author__name">{author}</p>
+              <p className="author__type">{tags[0]}</p>
+            </div>
+            <HiOutlineArrowNarrowRight size={30} />
+          </div>
         </div>
-      </div>
-      <div className="author-container" onClick={getQuotesBtnHandler}>
-        <div className="author">
-          <p className="author__name">{author}</p>
-          <p className="author__type">{tags[0]}</p>
-        </div>
-        <HiOutlineArrowNarrowRight size={30} />
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
